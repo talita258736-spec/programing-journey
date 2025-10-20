bc3ca0b3e0747e98fc80aa83acb08054a129beff
talita258736-spec <talita258736@student.smkn1kandeman.sch.id>
2025-10-20 15:26:25 +0700

    Add files via upload

---
+++ praktik6.2_TalitaZakiyatulM_XPPLG1/praktik6.2_TalitaZakiyatulM_XPPLG1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace praktik6._2_TalitaZakiyatulM_XPPLG1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Membuat array integer dengan 5 elemen
+            int[] angka = new int[5];
+
+            //Input elemen array dari keyboard 
+            Console.WriteLine("Masukkan nilai ke-1: ");
+            angka[0] = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Masukkan nilai ke-2: ");
+            angka[1] = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Masukkan nilai ke-3: ");
+            angka[2] = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Masukkan nilai ke-4: ");
+            angka[3] = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Masukkan nilai ke-5: ");
+            angka[4] = Convert.ToInt32(Console.ReadLine());
+
+            //Menampilkan isi array tanpa perulangan 
+            Console.WriteLine("\nData yang Anda masukkan:");
+            Console.WriteLine("Nilai ke-1: " + angka[0]);
+            Console.WriteLine("Nilai ke-2: " + angka[1]);
+            Console.WriteLine("Nilai ke-3: " + angka[2]);
+            Console.WriteLine("Nilai ke-4: " + angka[3]);
+            Console.WriteLine("Nilai ke-5: " + angka[4]);
+        }
+    }
+}
